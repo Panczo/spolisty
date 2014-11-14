9f58ae5b6aa6c8f9474556f02a7d8f17a7f243c0
Tomasz Panek <joachimsantana@gmail.com>
2014-11-14 13:49:56 +0100

    add header with responsive background image

--- spec/features/landing_pages_spec.rb
+++ spec/features/landing_pages_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 feature 'Landing page' do
 	scenario 'have name of application' do
 		visit root_path
-
-		expect(page).to have_content("Spolisty")
+		within("h1") do
+			expect(page).to have_content("SPOLISTY.COM")
+		end
 	end
 end
