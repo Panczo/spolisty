2113157949b673574d7266a485e4a10e59646320
Tomasz Panek <joachimsantana@gmail.com>
2014-11-20 14:17:01 +0100

    cleanup test folder

--- spec/features/landing_pages_spec.rb
+++ spec/features/landing_pages_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 feature 'Landing page' do
 	scenario 'have name of application' do
 		visit root_path
-		within("h1") do
-			expect(page).to have_content("SPOLISTY.COM")
+		within(".lead") do
+			expect(page).to have_content("Explore your music with friends")
 		end
 	end
 end
