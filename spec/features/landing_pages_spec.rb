2afdc4f1b9494f91e2344e24566fb6269d9a2175
Tomasz Panek <joachimsantana@gmail.com>
2014-11-20 14:41:55 +0100

    add test for successfull join to subscribed list

--- spec/features/landing_pages_spec.rb
+++ spec/features/landing_pages_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
 feature 'Landing page' do
-	scenario 'have name of application' do
+	before(:each) do
 		visit root_path
+	end
+
+	scenario 'have name of application' do	
 		within(".lead") do
 			expect(page).to have_content("Explore your music with friends")
 		end
+
+		within(".btn-cta", match: :first) do
+			expect(page).to have_content("CONNECT WITH SPOTIFY")
+		end
 	end
+
+	scenario 'successfull join to landing page' do
+		within("#subscribe") do
+			fill_in("email", with: "test@test.com")
+			click_button "SUBSCRIBE"
+		end
+
+		expect(page).to have_content("Successfully added to our mailing list.")
+	end
+
+
 end
