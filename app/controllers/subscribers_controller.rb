569299a72a26fe573e2e4f8eefe86d73de26af3d
Tomasz Panek <joachimsantana@gmail.com>
2014-11-20 13:51:38 +0100

    add controller and method for create subscribe with gibbon

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,28 +1,17 @@
 class SubscribersController < ApplicationController
 
 	def create
-
-
-		@list_id = ENV['MAILCHIMP_LIST']
-    gb = Gibbon::API.new
-
-    result = gb.lists.subscribe({
-      :id => @list_id,
-      :email => {:email => params[:email][:address]}
-      })
-
-
 		begin
-  		Gibbon::API.new(ENV['MAILCHIMP']).lists.subscribe({:id => ENV['MAILCHIMP_LIST'], :email => {:email => params[:email][:address]}, :double_optin => false})
-  		 flash[:success] = "Twój komentarz został dodany"   
+			@result = Gibbon::API.new(ENV['MAILCHIMP']).lists.subscribe(
+				{
+					:id => ENV['MAILCHIMP_LIST'], 
+					:email => {:email => params[:email][:address]}, 
+					:double_optin => false
+				})
+				flash[:success] = "#{@result}"
 		rescue Gibbon::MailChimpError => e
-		  return redirect_to root_path, :flash => { error: e.message }
+	  	return :flash => { error: e.message }
 		end
-
-		gb = Gibbon::API.new
-
-	  result = gb.lists.subscribe({:id => “campaign_list_id”, :email => email}, :double_optin => false})
-
-	  Rails.logger.error(“Error from MailChimp: #{result[‘error’]}”) if result[“status”] == “error”
 	end
+
 end
