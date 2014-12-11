31998d7ad986de0679fbc6b162f89c0a5c62c8f7
Tomasz Panek <joachimsantana@gmail.com>
2014-12-11 14:08:46 +0100

    cleanup views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
-	def is_active?(link_path)
-		current_page?(link_path) ? "active" : ""
-	end
+  def is_active?(link_path)
+    current_page?(link_path) ? "active" : ""
+  end
 
-	def foto(user)
-		user.image? ? image_tag(@user.image, class: 'img-circle img-thumbnail') : content_tag(:i, "", class: 'fa fa-user img-thumbnail img-circle ') 
-	end
+  def foto(user)
+    user.image? ? image_tag(@user.image, class: 'img-circle img-thumbnail') : content_tag(:i, "", class: 'fa fa-user img-thumbnail img-circle ') 
+  end
 end
