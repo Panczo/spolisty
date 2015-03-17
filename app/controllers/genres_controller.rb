1c1cb755e081e1ddadb57d8aad30433305d84339
Tomasz Panek <joachimsantana@gmail.com>
2015-03-17 11:01:50 +0100

    Styled user chart

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,9 @@
 class GenresController < ApplicationController
   before_action :authenticate_user! 
 
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
     resources :charts
   end
 
+  resources :genres, only: [:show]
   resources :subscribes, only: [:create]
   resources :pages, only: [:index]
 
