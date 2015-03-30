edc41aafcbaf455700ad65ab6b05b2e79e2cb6e4
Tomasz Panek <joachimsantana@gmail.com>
2015-03-30 08:36:28 +0200

    Styled form for user update accout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305115401) do
+ActiveRecord::Schema.define(version: 20150330061317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20150305115401) do
     t.string   "spotify_id"
     t.integer  "chartgeneratecount",     default: 0
     t.datetime "chartgeneratetime"
+    t.text     "about"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
