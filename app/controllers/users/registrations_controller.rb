e59ff13b3ddf1944400d10c705fa2f05740eceef
Tomasz Panek <joachimsantana@gmail.com>
2015-03-30 09:12:52 +0200

    allow user edit their own account without pass current_password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,4 +16,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def cancel
     super
   end
+
+  # Overwrite update_resource to let users to update their user without giving their password
+  def update_resource(resource, params)
+    if current_user.provider == "spotify"
+      params.delete("current_password")
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
 end
+
+
