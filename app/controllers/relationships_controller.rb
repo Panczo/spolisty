1b56f09c0189d3ea6a49994a4badf486fe82bcf3
Tomasz Panek <joachimsantana@gmail.com>
2015-04-02 10:51:23 +0200

    styled follow/unfollow buttons

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,17 @@
+class RelationshipsController < ApplicationController
+  before_action :authenticate_user! 
+
+  def create
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+    flash[:success] = "Now you follow #{user.nick}"
+    redirect_to user
+  end
+
+  def destroy
+    user = Relationship.find(params[:id]).followed
+    current_user.unfollow(user)
+    flash[:success] = "Now you unfollow #{user.nick}"
+    redirect_to user
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,14 @@ class UsersController < ApplicationController
   end
 
   def following
+    @title = 'following'
     @user = User.find(params[:id])
     @users = @user.following
     render 'show_follow'
   end
 
   def followers
+    @title = 'followers'
     @user = User.find(params[:id])
     @users = @user.followers
     render 'show_follow'
