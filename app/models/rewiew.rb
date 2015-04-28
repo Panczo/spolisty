83b700eb4d0b1b519803e8716f27cc8b95573fc1
Tomasz Panek <joachimsantana@gmail.com>
2015-04-28 12:02:34 +0200

    CLean up after rename rewiew model to review

--- app/models/rewiew.rb
+++
@@ -1,7 +0,0 @@
-class Rewiew < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :playlist
-
-  validates :user, :playlist, :rating, :comment, presence: true
-  validates :rating, numericality: { only_integer: true, greater_than: 0 }
-end
--- spec/factories/rewiews.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :rewiew do
-    rating 1
-comment "MyText"
-user_id 1
-playlist_id 1
-  end
-
-end
