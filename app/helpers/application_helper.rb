67122e70c18c4906cc0c171dcda05d5a0a733ce8
Tomasz Panek <joachimsantana@gmail.com>
2015-05-04 10:07:22 +0200

    Fix problem with not showing stars after create new review  && add better style to user review image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,16 @@ module ApplicationHelper
   def foto(user)
     unless user.blank?
       user.image? ? image_tag(user.image, class: 'img-circle img-thumbnail user_link') : content_tag(:i, "", class: 'fa fa-user img-thumbnail img-circle ') 
+    else
+      content_tag(:i, "", class: 'fa fa-user img-thumbnail img-circle ')
+    end
+  end
+
+  def review_user_nick(user)
+    if user.present?
+      user.nick
+    else
+      "User deleted account"
     end
   end
 end
