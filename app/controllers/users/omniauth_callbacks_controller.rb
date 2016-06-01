bbdbec4a4c65367018c6b05a2bf0a6ca66d5eba9
Tomasz Panek <joachimsantana@gmail.com>
2016-06-01 13:47:27 +0200

    Try to fix 404 Resource Not Found from rspotify

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,6 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       spotify_user = RSpotify::User.new(omnia_hash)
       @user.spotify_hash = spotify_user.to_hash
+      @user.save!
       sign_in_and_redirect @user, :event => :authentication 
       set_flash_message(:notice, :success, :kind => "Spotify") if is_navigational_format?
     else
